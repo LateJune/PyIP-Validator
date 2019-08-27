0ef7b7288b576eb2372c28f91afc071449331647
root <root@localhost.localdomain>
2019-08-27 11:31:54 -0400

    branch, made fucntion to invadlidate zero ips.. doesn't quite work

--- find_good_ip.py
+++ find_good_ip.py
@@ -1,4 +1,4 @@
-#!/bin/env python
+#!/usr/bin/env python
 import sys
 import re
 
@@ -15,6 +15,27 @@ def truthy(ip):
 
     return True
 
+def remove_zero_invalid(ip):
+    split_ip = ip.split('.')
+    for octet in split_ip:
+        if len(octet) == 3:
+            if octet[0] == '0' or octet[0] == '0' and octet[1] == '0' and octet[2] == '0' or octet[0] == '0' and octet[1] == '0' and octet[2] == '0' or octet[0] == '0' and int(octet[1]) > 0 or octet[0] == '0' and octet[1] == '0' and int(octet[2]) > 0:
+                return False  
+
+        elif len(octet) == 2:
+            if octet[0] == '0' or octet[0] == '0' and octet[1] == '0':
+                return False
+
+        elif len(octet) == 1:
+            if octet[0] == '0':
+                return False
+
+        else:
+            return False
+
+
+    return True
+
 def main():
     
     myfile = open(sys.argv[1], 'r')
@@ -26,14 +47,23 @@ def main():
         ip_match += re.findall(ipPattern, line)
     #print(ip_match)
     
+
     ip_dict = {}
     for ip in ip_match:
+        where_is_ip_in_list = 0
+
         if ip == "...":
             continue
+        if remove_zero_invalid(ip):
+            ip_match.pop(where_is_ip_in_list)
+
+        
         if not ip in ip_dict:
             ip_dict[ip] = 1
         else:
             ip_dict[ip] += 1
+        where_is_ip_in_list += 1
+
     #print(ip_dict)
 
     sorted_dict = sorted(ip_dict.items(), key=lambda kv: (kv[1], kv[0])) 
